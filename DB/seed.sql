11aaa5c92dfd31e429914dd4ece360037ffac0f0
Joonho Lee <joonholee@Joonhos-MacBook-Air.local>
2018-02-05 10:13:19 -0500

    finished controllers

---
+++ DB/seed.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUE ('cheese', true);
+INSERT INTO burgers (burger_name, devoured) VALUE ('jalapino', true);
+INSERT INTO burgers (burger_name, devoured) VALUE ('veggie', false);
