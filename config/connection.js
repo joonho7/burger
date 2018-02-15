3b5dcc191fe08f6e7e708865c3c097608485c783
Joonho Lee <joonholee@Joonhos-MacBook-Air.local>
2018-02-15 14:58:59 -0500

    heroku compatible

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,15 @@
 var mysql = require("mysql");
 
+if(process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}else{
 var connection = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "letmeinplease",
   database: "burgers_db"
-});
+ });
+}
 
 
 connection.connect(function(err) {
@@ -16,4 +20,5 @@ connection.connect(function(err) {
   console.log("connected as id " + connection.threadId);
 });
 
+connection.connect();
 module.exports = connection;
