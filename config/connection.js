4f4c7cac94cc7d311137e60c96ea4d51166e31a6
Joonho Lee <joonholee@Joonhos-MacBook-Air.local>
2018-02-16 12:26:57 -0500

    comment out connection

--- config/connection.js
+++ config/connection.js
@@ -12,13 +12,13 @@ var connection = mysql.createConnection({
 }
 
 
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
+// connection.connect(function(err) {
+//   if (err) {
+//     console.error("error connecting: " + err.stack);
+//     return;
+//   }
+//   console.log("connected as id " + connection.threadId);
+// });
 
 //connection.connect();
 module.exports = connection;
